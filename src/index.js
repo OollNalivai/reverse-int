880c073deed53df6ab33db17c1e6ac8275ea2f4d
VladislavOoll <apahiphip@mail.ru>
2021-03-10 23:42:03 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 module.exports = function reverse (n) {
-  
+    return (Math.sign(n) === 1 && n%10 !== 0)? +(`${n}`.split('').reverse().join('')) : +(`${Math.abs(n)}`.split('').reverse().join(''));
 }
